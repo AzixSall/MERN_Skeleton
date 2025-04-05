453854a43c609376e341c8be3231a0e253525c92
Abdou Aziz Sall <abdazizsall@outlook.com>
2025-04-05 22:35:21 +0300

    Get/update/Delete User 2

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,8 @@
 import User from '../models/user';
+import UserDocument from '../models/user';
 import errorHandler from '../helpers/dbErrorHandler';
 import { Request, Response, NextFunction } from 'express';
+import { extend } from 'lodash';
 
 
 
@@ -13,7 +15,7 @@ const create = async(req: Request, res: Response) => {
     catch(e){
         res.status(400).json({error: errorHandler.getErrorMessage(e)});
     }
-}
+};
 
 const list = async(req: Request, res :Response) =>{
     try{
@@ -25,10 +27,47 @@ const list = async(req: Request, res :Response) =>{
     }
 }
 
-
-const userById = (req : Request, res : Response, next : NextFunction, id : string) =>{};
-const read = (req : Request, res : Response) =>{};
-const update = (req : Request, res : Response, next :  NextFunction) =>{};
-const remove = (req : Request, res : Response, next : NextFunction) =>{};
+const userById = async(req : Request, res : Response, next : NextFunction, id : string) =>{
+    try {
+        const user = await User.findById(id);
+        if(!user){
+            res.status(200).json({message : 'User unkown'});
+        }
+        req.profile = user;
+        next();
+    }
+    catch (e){
+        res.status(500).json({error : 'Could not retrive user'});
+    }
+};
+const read = (req : Request, res : Response) =>{
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    res.json(req.profile);
+};
+const update = async(req : Request, res : Response, next :  NextFunction) =>{
+    try {
+        let user = req.profile;
+        user = extend(user, req.body);
+        await user.save();
+        req.profile = user;
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json(user);
+    } catch (error) {
+        res.status(400).json({error : errorHandler.getErrorMessage(error)});
+    }
+};
+const remove = async(req : Request, res : Response, next : NextFunction) =>{
+    try {
+        let user = req.profile;
+        let deletedUser = await user.remove();
+        deletedUser.hashed_password = undefined;
+        deletedUser.salt = undefined;
+        res.json(deletedUser);
+    } catch (error) {
+        res.status(400).json({error : errorHandler.getErrorMessage(error)});
+    }
+};
 
 export default{create, list, userById, read, update, remove};
